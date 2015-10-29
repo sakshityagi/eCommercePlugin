89000cb5d2a113737ca0d14420499360d0f7804c
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-10-29 16:31:59 +0530

    Added angular app structure in widget section. Renamed template files

---
+++ widget/controllers/widget.home.controller.js
@@ -0,0 +1,11 @@
+
+'use strict';
+
+(function (angular) {
+  angular
+    .module('eCommercePluginWidget')
+    .controller('WidgetHomeCtrl', ['$scope',
+      function ($scope) {
+
+      }]);
+})(window.angular);
