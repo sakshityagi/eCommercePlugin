ec461dd624f79ee4d2ddd6da8545eab7470fde9f
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-10-29 15:43:06 +0530

    Structured settings section

---
+++ control/settings/app.js
@@ -0,0 +1,9 @@
+'use strict';
+(function (angular) {
+  angular
+    .module('eCommercePluginSettings',[])
+    .controller('SettingsCtrl', ['$scope',
+      function ($scope) {
+
+      }]);
+})(window.angular);
