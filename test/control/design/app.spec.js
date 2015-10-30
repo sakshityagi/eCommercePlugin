126311ebbd63667ec11025ea673e2ada1fec6f1e
tabish.khan <tabish.khan@tothenew.com>
2015-10-31 01:11:30 +0530

    Added test environment for widget and design section.

---
+++ test/control/design/app.spec.js
@@ -0,0 +1,11 @@
+describe('Unit: eCommercePlugin widget app', function () {
+  describe('Unit: app routes', function () {
+    beforeEach(module('eCommercePluginDesign'));
+    var route, rootScope;
+    beforeEach(inject(function ( _$route_, _$rootScope_) {
+      route = _$route_;
+      rootScope = _$rootScope_;
+    }));
+
+    });
+});
