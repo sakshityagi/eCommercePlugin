e75fec669aa2b28ab80cd8f0f761c45838c7365b
tabish.khan <tabish.khan@tothenew.com>
2015-11-10 00:00:29 +0530

    Added some test cases of enum & Controller for  Content section.

--- test/control/content/content.home.controller.spec.js
+++ test/control/content/content.home.controller.spec.js
@@ -2,7 +2,7 @@ describe('Unit : eCommerce Plugin content.home.controller.js', function () {
     var ContentHome, scope, $rootScope, $controller, Buildfire, ActionItems, TAG_NAMES, STATUS_CODE, STATUS_MESSAGES, q;
     beforeEach(module('eCommercePluginContent'));
     var editor;
-    beforeEach(inject(function (_$rootScope_, _$q_, _$controller_, _TAG_NAMES_, _STATUS_CODE_, _STATUS_MESSAGES_) {
+    beforeEach(inject(function (_$rootScope_, _$q_, _$controller_, _TAG_NAMES_, _STATUS_CODE_, _LAYOUTS_, _STATUS_MESSAGES_) {
         $rootScope = _$rootScope_;
         q = _$q_;
         scope = $rootScope.$new();
@@ -10,7 +10,25 @@ describe('Unit : eCommerce Plugin content.home.controller.js', function () {
         TAG_NAMES = _TAG_NAMES_;
         STATUS_CODE = _STATUS_CODE_;
         STATUS_MESSAGES = _STATUS_MESSAGES_;
-     }));
+        LAYOUTS = _LAYOUTS_;
+        // Utils = __Utils__;
+        Buildfire = {
+            components: {
+                carousel: {
+                    editor: function (name) {
+                        return {}
+                    },
+                    viewer: function (name) {
+                        return {}
+                    }
+                }
+            }
+        };
+        ActionItems = jasmine.createSpyObj('ActionItems', ['showDialog']);
+        Utils = jasmine.createSpyObj('Utils', ['validLongLats']);
+        Buildfire.components.carousel = jasmine.createSpyObj('Buildfire.components.carousel', ['editor', 'onAddItems']);
+
+    }));
 
     beforeEach(function () {
         ContentHome = $controller('ContentHomeCtrl', {
@@ -19,9 +37,25 @@ describe('Unit : eCommerce Plugin content.home.controller.js', function () {
             Buildfire: Buildfire,
             TAG_NAMES: TAG_NAMES,
             ActionItems: ActionItems,
-            STATUS_CODE: STATUS_CODE
+            STATUS_CODE: STATUS_CODE,
+            LAYOUTS: LAYOUTS,
+            Utils: Utils
         });
     });
 
+    describe('Units: units should be Defined', function () {
+        it('it should pass if ContentHome is defined', function () {
+            expect(ContentHome).not.toBeUndefined();
+        });
+        it('it should pass if Buildfire is defined', function () {
+            expect(Buildfire).not.toBeUndefined();
+        });
+        it('it should pass if TAG_NAMES is defined', function () {
+            expect(TAG_NAMES).not.toBeUndefined();
+        });
+        it('it should pass if STATUS_CODE is defined', function () {
+            expect(STATUS_CODE).not.toBeUndefined();
+        });
+    });
 })
 ;
