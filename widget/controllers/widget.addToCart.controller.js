a052a3a30165dc9f1caa16b311fb666b660f6692
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-11-19 16:33:31 +0530

    Implemented redirecting to shopping cart page

--- widget/controllers/widget.addToCart.controller.js
+++ widget/controllers/widget.addToCart.controller.js
@@ -1,143 +1,149 @@
 'use strict';
 
 (function (angular) {
-    angular
-        .module('eCommercePluginWidget')
-        .controller('WidgetaddToCartCtrl', ['$scope', 'DataStore', 'TAG_NAMES', 'ECommerceSDK', '$sce', 'LAYOUTS', '$rootScope', 'PAGINATION', 'Buildfire', 'ViewStack',
-            function ($scope, DataStore, TAG_NAMES, ECommerceSDK, $sce, LAYOUTS, $rootScope, PAGINATION, Buildfire, ViewStack) {
+  angular
+    .module('eCommercePluginWidget')
+    .controller('WidgetaddToCartCtrl', ['$scope', 'DataStore', 'TAG_NAMES', 'ECommerceSDK', '$sce', 'LAYOUTS', '$rootScope', 'PAGINATION', 'Buildfire', 'ViewStack',
+      function ($scope, DataStore, TAG_NAMES, ECommerceSDK, $sce, LAYOUTS, $rootScope, PAGINATION, Buildfire, ViewStack) {
 
-               var WidgetAddToCart = this;
-                WidgetAddToCart.listeners = {};
-                WidgetAddToCart.currentAddedItemInCart={
-                    Variant:null
-                    }
-                var currentView = ViewStack.getCurrentView();
-                console.log("currentView",currentView)
-                var currentStoreName = "";
+        var WidgetAddToCart = this;
+        WidgetAddToCart.listeners = {};
+        WidgetAddToCart.currentAddedItemInCart = {
+          Variant: null
+        };
+        var currentView = ViewStack.getCurrentView();
+        console.log("currentView", currentView);
+        var currentStoreName = "";
 
-                WidgetAddToCart.safeHtml = function (html) {
-                    if (html)
-                        return $sce.trustAsHtml(html);
-                }
+        WidgetAddToCart.safeHtml = function (html) {
+          if (html)
+            return $sce.trustAsHtml(html);
+        };
 
-                var getProduct = function (storeName, handle) {
-                    Buildfire.spinner.show();
-                    var success = function (result) {
-                            Buildfire.spinner.hide();
-                            console.log("===============================", result);
-                            WidgetAddToCart.item = result;
-                            console.log("WidgetAddToCart",WidgetAddToCart)
-                        }
-                        , error = function (err) {
-                            Buildfire.spinner.hide();
-                            console.error('Error In Fetching Single product Details', err);
-                        };
-                    ECommerceSDK.getProduct(storeName, handle).then(success, error);
-                };
+        var getProduct = function (storeName, handle) {
+          Buildfire.spinner.show();
+          var success = function (result) {
+              Buildfire.spinner.hide();
+              console.log("===============================", result);
+              WidgetAddToCart.item = result;
+              console.log("WidgetAddToCart", WidgetAddToCart)
+            }
+            , error = function (err) {
+              Buildfire.spinner.hide();
+              console.error('Error In Fetching Single product Details', err);
+            };
+          ECommerceSDK.getProduct(storeName, handle).then(success, error);
+        };
 
-                var init = function () {
-                    var success = function (result) {
-                            WidgetAddToCart.data = result.data;
-                            if (!WidgetAddToCart.data.design)
-                                WidgetAddToCart.data.design = {};
-                            if (!WidgetAddToCart.data.content)
-                                WidgetAddToCart.data.content = {};
-                            if (!WidgetAddToCart.data.settings)
-                                WidgetAddToCart.data.settings = {};
-                            if (WidgetAddToCart.data.content.storeName) {
-                                currentStoreName = WidgetAddToCart.data.content.storeName;
-                            }
-                            if (!WidgetAddToCart.data.design.itemListLayout) {
-                                WidgetAddToCart.data.design.itemListLayout = LAYOUTS.itemListLayout[0].name;
-                            }
-                            if (WidgetAddToCart.data.content.storeName && currentView.params.handle)
-                                getProduct(WidgetAddToCart.data.content.storeName, currentView.params.handle);
-                        }
-                        , error = function (err) {
-                            console.error('Error while getting data', err);
-                        };
-                    DataStore.get(TAG_NAMES.SHOPIFY_INFO).then(success, error);
-                };
+        var init = function () {
+          var success = function (result) {
+              WidgetAddToCart.data = result.data;
+              if (!WidgetAddToCart.data.design)
+                WidgetAddToCart.data.design = {};
+              if (!WidgetAddToCart.data.content)
+                WidgetAddToCart.data.content = {};
+              if (!WidgetAddToCart.data.settings)
+                WidgetAddToCart.data.settings = {};
+              if (WidgetAddToCart.data.content.storeName) {
+                currentStoreName = WidgetAddToCart.data.content.storeName;
+              }
+              if (!WidgetAddToCart.data.design.itemListLayout) {
+                WidgetAddToCart.data.design.itemListLayout = LAYOUTS.itemListLayout[0].name;
+              }
+              if (WidgetAddToCart.data.content.storeName && currentView.params.handle)
+                getProduct(WidgetAddToCart.data.content.storeName, currentView.params.handle);
+            }
+            , error = function (err) {
+              console.error('Error while getting data', err);
+            };
+          DataStore.get(TAG_NAMES.SHOPIFY_INFO).then(success, error);
+        };
 
-                var onUpdateCallback = function (event) {
-                    setTimeout(function () {
-                        if (event && event.tag) {
-                            switch (event.tag) {
-                                case TAG_NAMES.SHOPIFY_INFO:
-                                    WidgetAddToCart.data = event.data;
-                                    if (!WidgetAddToCart.data.design)
-                                        WidgetAddToCart.data.design = {};
-                                    if (!WidgetAddToCart.data.design.itemListLayout) {
-                                        WidgetAddToCart.data.design.itemListLayout = LAYOUTS.itemListLayout[0].name;
-                                    }
-                                    if (!WidgetAddToCart.data.content.storeName) {
-                                        WidgetAddToCart.item = null;
-                                        currentStoreName = "";
-                                    }
-
-                                    if (WidgetAddToCart.data.content.storeName && currentStoreName != WidgetAddToCart.data.content.storeName) {
-                                        WidgetAddToCart.item = null;
-                                        getProduct(WidgetAddToCart.data.content.storeName, currentView.params.handle);
-                                    }
-                                    if (!WidgetAddToCart.data.content.storeName)
-                                        ViewStack.popAllViews();
-                                    break;
-                            }
-                            $scope.$digest();
-                        }
-                    }, 0);
-                };
-
-                /**
-                 * DataStore.onUpdate() is bound to listen any changes in datastore
-                 */
-                DataStore.onUpdate().then(null, null, onUpdateCallback);
-
-                WidgetAddToCart.selectVariant = function(variant){
-                    WidgetAddToCart.currentAddedItemInCart.Variant = variant;
-                    console.log("WidgetAddToCart.currentAddedItemInCart.Variant",WidgetAddToCart.currentAddedItemInCart.Variant)
+        var onUpdateCallback = function (event) {
+          setTimeout(function () {
+            if (event && event.tag) {
+              switch (event.tag) {
+                case TAG_NAMES.SHOPIFY_INFO:
+                  WidgetAddToCart.data = event.data;
+                  if (!WidgetAddToCart.data.design)
+                    WidgetAddToCart.data.design = {};
+                  if (!WidgetAddToCart.data.design.itemListLayout) {
+                    WidgetAddToCart.data.design.itemListLayout = LAYOUTS.itemListLayout[0].name;
+                  }
+                  if (!WidgetAddToCart.data.content.storeName) {
+                    WidgetAddToCart.item = null;
+                    currentStoreName = "";
                   }
 
-                WidgetAddToCart.proceedToCart = function(handle)
-                {
-                    ViewStack.push({
-                        template: 'Add_To_Cart_2',
-                        params: {
-                            handle: handle
-                        }
-                    });
-                }
-                $scope.$on("$destroy", function () {
-                    for (var i in WidgetAddToCart.listeners) {
-                        if(WidgetAddToCart.listeners.hasOwnProperty(i)) {
-                            WidgetAddToCart.listeners[i]();
-                        }
-                    }
-                    DataStore.clearListener();
-                });
-
-                WidgetAddToCart.listeners['CAROUSEL_LOADED'] = $rootScope.$on("Carousel3:LOADED", function () {
-                    WidgetAddToCart.view = null;
-                    if (!WidgetAddToCart.view) {
-                        WidgetAddToCart.view = new buildfire.components.carousel.view("#carousel3", [], "WideScreen");
-                    }
-                    if (WidgetAddToCart.item && WidgetAddToCart.item.images) {
-                        var imageArray = WidgetAddToCart.item.images.map(function (item) {
-                            return {iconUrl: item.src, title : ""};
-                        });
-                         WidgetAddToCart.view.loadItems(imageArray, null, "WideScreen");
-                    } else {
-                        WidgetAddToCart.view.loadItems([]);
-                    }
-                });
-
-                WidgetAddToCart.listeners['POP'] = $rootScope.$on('BEFORE_POP', function (e, view) {
-                    console.log("SINGLE:", view.template, 'Add_To_Cart_1');
-                    if(view.template === 'Add_To_Cart_1') {
-                        $scope.$destroy();
-                    }
-                });
-                init();
+                  if (WidgetAddToCart.data.content.storeName && currentStoreName != WidgetAddToCart.data.content.storeName) {
+                    WidgetAddToCart.item = null;
+                    getProduct(WidgetAddToCart.data.content.storeName, currentView.params.handle);
+                  }
+                  if (!WidgetAddToCart.data.content.storeName)
+                    ViewStack.popAllViews();
+                  break;
+              }
+              $scope.$digest();
             }
-        ])
+          }, 0);
+        };
+
+        /**
+         * DataStore.onUpdate() is bound to listen any changes in datastore
+         */
+        DataStore.onUpdate().then(null, null, onUpdateCallback);
+
+        WidgetAddToCart.selectVariant = function (variant) {
+          WidgetAddToCart.currentAddedItemInCart.Variant = variant;
+          console.log("WidgetAddToCart.currentAddedItemInCart.Variant", WidgetAddToCart.currentAddedItemInCart.Variant)
+        };
+
+        WidgetAddToCart.proceedToCart = function (handle) {
+          ViewStack.push({
+            template: 'Add_To_Cart_2',
+            params: {
+              handle: handle
+            }
+          });
+        };
+
+        WidgetAddToCart.addProductToCart = function () {
+          ViewStack.push({
+            template: 'Shopping_Cart'
+          });
+        };
+
+        $scope.$on("$destroy", function () {
+          for (var i in WidgetAddToCart.listeners) {
+            if (WidgetAddToCart.listeners.hasOwnProperty(i)) {
+              WidgetAddToCart.listeners[i]();
+            }
+          }
+          DataStore.clearListener();
+        });
+
+        WidgetAddToCart.listeners['CAROUSEL_LOADED'] = $rootScope.$on("Carousel3:LOADED", function () {
+          WidgetAddToCart.view = null;
+          if (!WidgetAddToCart.view) {
+            WidgetAddToCart.view = new buildfire.components.carousel.view("#carousel3", [], "WideScreen");
+          }
+          if (WidgetAddToCart.item && WidgetAddToCart.item.images) {
+            var imageArray = WidgetAddToCart.item.images.map(function (item) {
+              return {iconUrl: item.src, title: ""};
+            });
+            WidgetAddToCart.view.loadItems(imageArray, null, "WideScreen");
+          } else {
+            WidgetAddToCart.view.loadItems([]);
+          }
+        });
+
+        WidgetAddToCart.listeners['POP'] = $rootScope.$on('BEFORE_POP', function (e, view) {
+          console.log("SINGLE:", view.template, 'Add_To_Cart_1');
+          if (view.template === 'Add_To_Cart_1') {
+            $scope.$destroy();
+          }
+        });
+        init();
+      }
+    ])
 })(window.angular);
