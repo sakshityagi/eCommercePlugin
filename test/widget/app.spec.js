d75fb77365dc406ca2660f1303f50cce4c32b690
tabish.khan <tabish.khan@tothenew.com>
2015-11-19 22:22:17 +0530

    Added test cases for widget section.

--- test/widget/app.spec.js
+++ test/widget/app.spec.js
@@ -1,22 +1,43 @@
 describe('Unit: eCommercePluginWidget widget app', function () {
   describe('Unit: app routes', function () {
     beforeEach(module('eCommercePluginWidget'));
-    var location, route, rootScope;
-    beforeEach(inject(function (_$location_, _$route_, _$rootScope_) {
-      location = _$location_;
-      route = _$route_;
+    var location, route, rootScope, compile, scope;
+    beforeEach(inject(function (_$rootScope_, _$compile_, $rootScope) {
+     // route = _$route_;
       rootScope = _$rootScope_;
+        compile= _$compile_;
+        scope = $rootScope.$new();
+        directiveElem = getCompiledElement();
     }));
 
     describe('Home route', function () {
       beforeEach(inject(
           function ($httpBackend) {
-            $httpBackend.expectGET('templates/home.html')
+            $httpBackend.expectGET('/')
                 .respond(200);
             $httpBackend.expectGET('/')
                 .respond(200);
           }));
+      it('should load the home page on successful load of location path /', function () {
+
+      });
 
     });
+
+      function getCompiledElement(){
+          var element = angular.element('<div id="carousel" build-fire-carousel="></div>');
+          var compiledElement = compile(element)(scope);
+          scope.$digest();
+          return compiledElement;
+      }
+      it('should call if build-fire-carousel is defined', function () {
+          expect(directiveElem.find('build-fire-carousel')).toBeDefined();
+      });
+      it('should call if build-fire-carousel2 is defined', function () {
+          expect(directiveElem.find('build-fire-carousel2')).toBeDefined();
+      });
+      it('should call if build-fire-carousel3 is defined', function () {
+          expect(directiveElem.find('build-fire-carousel3')).toBeDefined();
+      });
   });
 });
