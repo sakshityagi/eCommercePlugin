ad322e9d1816a4fb509395abe84e84143f048a2b
tabish.khan <tabish.khan@tothenew.com>
2015-11-21 00:41:26 +0530

    Added background image in all the layouts in widget section.

--- widget/app.js
+++ widget/app.js
@@ -95,7 +95,17 @@
 
           }
         };
-      }]).filter('cropImage', [function () {
+      }]).directive('backImg', function(){
+        return function(scope, element, attrs){
+          attrs.$observe('backImg', function(value) {
+            element.css({
+              'background': 'url(' + value +')',
+              'background-size' : 'cover',
+              'background-color': 'white'
+            });
+          });
+        };
+      }).filter('cropImage', [function () {
       return function (url, width, height, noDefault) {
         if (noDefault) {
           if (!url)
